b9df5885e01f5071e2586cccf643eaa5d4ae5b7e
yellow2041 <jeeyeon654@gmail.com>
2022-08-15 16:33:45 +0900

    [chore] 초기 커밋

---
+++ src/main/java/com/example/springcloudmsa/SpringCloudMsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcloudmsa/SpringCloudMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
